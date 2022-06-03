3c4d8ccbebdbaf18c90b07fe491e316885ec6187
nick <gucun@qq.com>
2022-06-03 11:43:53 +0800

    test

--- ch2/main.go
+++ ch2/main.go
@@ -11,9 +11,10 @@ func main() {
 	// router := gin.Default()
 	router := gin.Default()
 	router.GET("healthz", func(c *gin.Context) {
-		c.String(http.StatusOK, "200", c.Request.Header)
+		c.String(http.StatusOK, "200")
 
 	})
+
 	router.Run()
 
 }
