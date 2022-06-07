eb307964def1e5d6b0e36c324aa09ce8a36b5d97
nick <gucun@qq.com>
2022-06-07 17:23:23 +0800

    httpserver

--- ch2/main.go
+++ ch2/main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"fmt"
 	"log"
+	"net"
 	"net/http"
+	"net/http/pprof"
 	"os"
 	"strings"
 )
@@ -13,6 +15,28 @@ import (
 // Server 端记录访问日志包括客户端 IP，HTTP 返回码，输出到 server 端的标准输出
 // 当访问 localhost/healthz 时，应返回 200
 
+func main() {
+
+	log.Println("Starting HTTP server...")
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", hello)
+	mux.HandleFunc("/healthz", healthz)
+	mux.HandleFunc("/debug/pprof/", pprof.Index)
+	mux.HandleFunc("/debug/pprof/profile", pprof.Profile)
+	mux.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
+	mux.HandleFunc("/debug/pprof/trace", pprof.Trace)
+
+	if err := http.ListenAndServe(":8080", mux); err != nil {
+		log.Fatalf("http error is %s", err.Error())
+	}
+
+}
+func hello(w http.ResponseWriter, req *http.Request) {
+	fmt.Println("hello")
+	fmt.Fprintf(w, "hello")
+}
+
 func healthz(w http.ResponseWriter, req *http.Request) {
 	for k, v := range req.Header {
 
@@ -20,15 +44,41 @@ func healthz(w http.ResponseWriter, req *http.Request) {
 
 	}
 	w.Header().Set("version", os.Getenv("version"))
-	fmt.Printf("%s\n", req.RemoteAddr)
+
+	// ip := clientip(req)
+	fmt.Printf("%s\n", clientip(req))
 	fmt.Fprintf(w, "200")
 
 }
 
-func main() {
+// FromRequest return client's real public IP address from http request headers.
+func clientip(r *http.Request) string {
+	// Fetch header value
+	xRealIP := r.Header.Get("X-Real-Ip")
+	xForwardedFor := r.Header.Get("X-Forwarded-For")
 
-	http.HandleFunc("/healthz", healthz)
+	// If both empty, return IP from remote address
+	if xRealIP == "" && xForwardedFor == "" {
+		var remoteIP string
 
-	log.Fatal(http.ListenAndServe("localhost:8080", nil))
+		// If there are colon in remote address, remove the port number
+		// otherwise, return remote address as is
+		if strings.ContainsRune(r.RemoteAddr, ':') {
+			remoteIP, _, _ = net.SplitHostPort(r.RemoteAddr)
+		} else {
+			remoteIP = r.RemoteAddr
+		}
 
+		return remoteIP
+	}
+
+	// Check list of IP in X-Forwarded-For and return the first global address
+	for _, address := range strings.Split(xForwardedFor, ",") {
+		address = strings.TrimSpace(address)
+
+		return address
+	}
+
+	// If nothing succeed, return X-Real-IP
+	return xRealIP
 }
